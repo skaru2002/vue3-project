3b1f71d61d60851ce924d1f1f47e6906047b1d88
meayang <skaru2002@naver.com>
2022-02-02 00:19:40 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>{{name}}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    const name = 'skaru2002';
+    return {
+      name
+    };
   }
 }
 </script>
