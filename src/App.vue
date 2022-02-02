f83167652862dcdef0a3d308c9a59270989928d9
meayang <skaru2002@naver.com>
2022-02-02 17:45:41 +0900

    event modifier

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,55 @@
 <template>
-  <div>{{name}}</div>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form
+      @submit.prevent="onSubmit" 
+      class="d-flex"
+    >
+      <div class="flex-glow-1 mr-2">
+        <input 
+          class="form-control"
+          type="text" 
+          v-model="todo"
+          placeholder="Type New Todo"
+        >
+      </div>
+      <div>
+        <button 
+          class="btn btn-primary" 
+          type="submit"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+    {{ todos }}
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   setup () {
-    const name = 'skaru2002';
+    const todo = ref('');
+    const todos = ref([]);
+
+    const onSubmit = () => {
+      todos.value.push({
+        id: Date.now(),
+        value: todo.value,
+      })
+    }
     return {
-      name
+      todo,
+      todos,
+      onSubmit,
     };
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.hi {
+  color: red;
 }
 </style>
