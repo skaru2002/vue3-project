b8b6c8ef9efc0b94306907aa68d1e1a702d50271
skaru2002 <skaru2002@smu.ac.kr>
2022-02-15 19:57:05 +0900

    집가는중

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       
-      <router-link class="navbar-brand" to="/">Navbar</router-link>
+      <router-link class="navbar-brand" :to="{name:'Home'}">Navbar</router-link>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            <router-link class="nav-link active" aria-current="page" :to="{name:'Home'}">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/Todos">Todos</router-link>
+            <router-link class="nav-link" :to="{name:'Todos'}">Todos</router-link>
           </li>
         </ul>
         <form class="d-flex">
